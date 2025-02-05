d465643535a72eb2773ef361cf555fd54f3b8cdb
ericorozco <eorozco@harmonytx.org>
2025-02-05 13:36:12 -0600

    Pinpoint
    Changed odometry method to use Pinpoint rather than 3-wheel odometry

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -3,20 +3,33 @@ package pedroPathing.constants;
 import com.pedropathing.localization.*;
 import com.pedropathing.localization.constants.*;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 public class LConstants {
     static {
-        ThreeWheelConstants.forwardTicksToInches = .001989436789;
+       /* ThreeWheelConstants.forwardTicksToInches = .001989436789;
         ThreeWheelConstants.strafeTicksToInches = .001989436789;
         ThreeWheelConstants.turnTicksToInches = .001989436789;
-        ThreeWheelConstants.leftY = 1;
-        ThreeWheelConstants.rightY = -1;
-        ThreeWheelConstants.strafeX = -2.5;
-        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftFront";
-        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightRear";
-        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
+        ThreeWheelConstants.leftY = 5.60;
+        ThreeWheelConstants.rightY = -5.60;
+        ThreeWheelConstants.strafeX = -5.25;
+        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftOdometer";
+        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightOdometer";
+        ThreeWheelConstants.strafeEncoder_HardwareMapName = "leftRear";
         ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
         ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
-        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;*/
+        PinpointConstants.forwardY = 5.60;
+        PinpointConstants.strafeX = -5.25;
+        PinpointConstants.distanceUnit = DistanceUnit.INCH;
+        PinpointConstants.hardwareMapName = "pinpoint";
+        PinpointConstants.useYawScalar = false;
+        PinpointConstants.yawScalar = 1.0;
+        PinpointConstants.useCustomEncoderResolution = false;
+        PinpointConstants.encoderResolution = GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD;
+        PinpointConstants.customEncoderResolution = 13.26291192;
+        PinpointConstants.forwardEncoderDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
+        PinpointConstants.strafeEncoderDirection = GoBildaPinpointDriver.EncoderDirection.REVERSED;
     }
 }
 
