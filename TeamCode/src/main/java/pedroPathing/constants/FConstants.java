f28c6b15c5e76af1ac7a86b0e88def7fe2c859fa
jerryel3ro <155690774+jerryel3ro@users.noreply.github.com>
2025-02-06 08:33:42 -0600

    teleop

--- TeamCode/src/main/java/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/FConstants.java
@@ -25,8 +25,8 @@ public class FConstants {
         FollowerConstants.xMovement = 55.25689538950779;
         FollowerConstants.yMovement = 39.49660558623038;
 
-        FollowerConstants.forwardZeroPowerAcceleration = -41.278;
-        FollowerConstants.lateralZeroPowerAcceleration = -59.7819;
+        FollowerConstants.forwardZeroPowerAcceleration = -33.4189;
+        FollowerConstants.lateralZeroPowerAcceleration = -59.8063;
 
         FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0);
         FollowerConstants.useSecondaryTranslationalPID = false;
@@ -36,7 +36,7 @@ public class FConstants {
         FollowerConstants.useSecondaryHeadingPID = false;
         FollowerConstants.secondaryHeadingPIDFCoefficients.setCoefficients(2,0,0.1,0); // Not being used, @see useSecondaryHeadingPID
 
-        FollowerConstants.drivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0);
+        FollowerConstants.drivePIDFCoefficients.setCoefficients(0.1,0.1,0,0.1,0);
         FollowerConstants.useSecondaryDrivePID = false;
         FollowerConstants.secondaryDrivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0); // Not being used, @see useSecondaryDrivePID
 
